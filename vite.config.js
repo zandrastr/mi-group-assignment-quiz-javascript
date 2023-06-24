61b913d8e00e0b232dada5299baabc9989a47b87
Sandra S <zandrastr@hotmail.com>
2023-06-24 11:01:03 +0200

    update vite config file

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
 import { defineConfig } from 'vite';
 
-/*
-Om ditt repo har adressen https://github.com/Medieinstitutet/fed22d-js-grundkurs-jenni-wumpus,
-då ska "base" här nedan vara "/fed22d-js-grundkurs-jenni-wumpus/"
- */
-
 export default defineConfig({
-  base: '/fed22d-js-grundkurs-2-grupprojekt-quizgruppen/', // TODO - ändra till ditt repo-namn
-  define: {
-    'process.env': {},
-  },
+  base: '/', 
   build: {
     target: 'esnext',
   },
